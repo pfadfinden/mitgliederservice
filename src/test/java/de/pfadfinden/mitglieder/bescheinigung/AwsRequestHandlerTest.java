214739a2673602cd916ee287c24e78f7b596950a
philippst <philipp.steinmetzger@comansy.de>
2017-10-03 18:12:05 +0200

    added basic testing

---
+++ src/test/java/de/pfadfinden/mitglieder/bescheinigung/AwsRequestHandlerTest.java
@@ -0,0 +1,68 @@
+package de.pfadfinden.mitglieder.bescheinigung;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import de.pfadfinden.mitglieder.bescheinigung.model.ValidationRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class AwsRequestHandlerTest {
+
+    AwsRequestHandler awsRequestHandler;
+
+    @BeforeEach
+    public void tearUp() {
+        this.awsRequestHandler = new AwsRequestHandler();
+    }
+
+    @Test
+    public void handleRequestValid() throws Exception {
+
+        Gson gson = new Gson();
+
+        ValidationRequest validationRequest = new ValidationRequest();
+        validationRequest.setFirstName("Jhilioo");
+        validationRequest.setLastName("Uteiumetzgeö");
+        validationRequest.setDateOfBirth("1987-08-13");
+        validationRequest.setMembershipNumber(11111);
+        validationRequest.setChallenge("XXXXXXXXXXXXXXX");
+
+        APIGatewayProxyRequestEvent apiGatewayRequest = new APIGatewayProxyRequestEvent();
+        apiGatewayRequest.setBody(gson.toJson(validationRequest));
+
+        APIGatewayProxyResponseEvent response = this.awsRequestHandler.handleRequest(apiGatewayRequest, null);
+
+        assertEquals(200, response.getStatusCode().intValue());
+        assertEquals("application/pdf",response.getHeaders().get("Content-Type"));
+        assertEquals("X-Amzn-Requestid",response.getHeaders().get("Access-Control-Expose-Headers"));
+        assertNotNull(response.getHeaders().get("Access-Control-Allow-Origin"));
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void handleRequestInvalid() throws Exception {
+
+        Gson gson = new Gson();
+
+        ValidationRequest validationRequest = new ValidationRequest();
+        validationRequest.setFirstName("Max");
+        validationRequest.setLastName("Uteiumetzgeö");
+        validationRequest.setDateOfBirth("1987-08-13");
+        validationRequest.setMembershipNumber(11111);
+        validationRequest.setChallenge("XXXXXXXXXXXXXXX");
+
+        APIGatewayProxyRequestEvent apiGatewayRequest = new APIGatewayProxyRequestEvent();
+        apiGatewayRequest.setBody(gson.toJson(validationRequest));
+
+        APIGatewayProxyResponseEvent response = this.awsRequestHandler.handleRequest(apiGatewayRequest, null);
+
+        assertEquals(400, response.getStatusCode().intValue());
+        assertNull(response.getBody());
+    }
+
+}
